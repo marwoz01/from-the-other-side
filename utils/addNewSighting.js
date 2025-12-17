3d11854759899985d1e429e351874477b2fdac72
Marcel <marwozniak01@gmail.com>
2025-12-17 17:28:21 +0100

    quick fixes

--- utils/addNewSighting.js
+++ utils/addNewSighting.js
@@ -1,30 +1,16 @@
-import path from 'node:path'
-import fs from 'node:fs/promises'
-import { getData } from './getData.js'
+import path from "node:path";
+import fs from "node:fs/promises";
+import { getData } from "./getData.js";
 
 export async function addNewSighting(newSighting) {
+  try {
+    const sightings = await getData();
+    sightings.push(newSighting);
 
-  try { 
+    const pathJSON = path.join("data", "data.json");
 
-    const sightings = await getData()
-    sightings.push(newSighting)
-    
-    const pathJSON = path.join('data', 'data.json')
-    
-    await fs.writeFile(
-      pathJSON,
-      JSON.stringify(sightings, null, 2),
-      'utf8'
-    )
-/*
-
-    4. Add the new sighting and check out the 'read' page.
-    
-    Bonus: figure out how to prettify the JSON!
-    Remember to uncomment the import statement in routeHandler.js!!
-*/
+    await fs.writeFile(pathJSON, JSON.stringify(sightings, null, 2), "utf8");
   } catch (err) {
-    throw new Error(err)
+    throw new Error(err);
   }
-
 }
